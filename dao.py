bf8deb312254669833dbcd0a02aba8c0906f21d2
vancongtuan1907@gmail <vancongtuan1907@gmail.com>
2024-03-07 15:16:54 +0700

    21_VanCongTuan Bo Sung File

--- dao.py
+++ dao.py
@@ -4,11 +4,3 @@ def load_categories():
     return Category.query.all()
 
 
-def load_products(kw=None):
-    products = Product.query
-
-    if kw:
-        products = products.filter(Product.name.contains(kw))
-
-    return products.all()
-
